1ea1f672d1f47c18ddd3ee94c547380df7112d82
getgiao <2648900364@qq.com>
2023-09-05 09:48:39 +0800

    too

--- dome.js
+++ dome.js
@@ -1,1 +1,13 @@
-console.log("one dome");
+// ==UserScript==
+// @name         测试0.1
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  没有描述
+// @author       mood
+// @match        https://www.bilibili.com/*
+// @icon         https://api.94speed.com/img/bitbug_favicon.ico
+// @grant        none
+// ==/UserScript==
+(function () {
+  "use strict";
+})();
